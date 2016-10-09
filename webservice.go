8ec1c0fba46255c688ec4080ee92ce57d492e521
Jesse Roberts <jesse@hackedpotatoes.com>
2016-10-08 23:27:35 -0600

    nice json response

--- webservice.go
+++ webservice.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/satori/go.uuid"
 	"net/http"
+	"time"
 )
+
+type UUIDResponse struct {
+	UUID      string `json:"uuid,omitempty"`
+	Timestamp string `json:"timestamp,omitempty"`
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Index")
 }
 
 func generateUUID(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, uuid.NewV4().String())
+	var u UUIDResponse
+	u.UUID = uuid.NewV4().String()
+	u.Timestamp = time.Now().Format(time.RFC822)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(u)
 }
 
 func SetHandlers() {
